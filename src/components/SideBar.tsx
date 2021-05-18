ab49f43b814356f6f4d7f0aeec41e8de67a34449
theycallmewolf <bruno.m.lobato@gmail.com>
2021-05-18 17:21:35 +0100

    adds sidebar component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,27 @@
+import { Button } from './Button';
+import { useMovies } from '../hooks/useMovies';
+
 export function SideBar() {
-  // Complete aqui
+  const { genres, selectedGenreId, setSelectedGenreId } = useMovies();
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
